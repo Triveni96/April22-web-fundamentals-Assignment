38840f3a81af01d8e282bff115ec6d3804c73b48
Triveni96 <98385893+Triveni96@users.noreply.github.com>
2022-05-30 13:40:58 +0530

    Add files via upload
    Java script test

---
+++ shape.js
@@ -0,0 +1,11 @@
+let length=10;
+let breadth=20;
+
+if(length == breadth)
+{
+    console.log("Square");
+}
+else
+{
+    console.log("Rectangle");
+}
---
+++ switch_statement.js
@@ -0,0 +1,36 @@
+let day1 = "sunday"
+let day2 = "Monday"
+let day3 = "Tuesday"
+let day4 = "wednessday"
+let day5 = "thursday"
+let day6 = "friday"
+let day7 = "saturday"
+
+let option = day2;
+switch(option)
+{
+    case day1:
+            console.log("sunday");
+            break;
+    case day2:
+            console.log("monday");
+            break;
+    case day3:
+            console.log("Tuesday");
+            break;
+    case day4:
+            console.log("Wednessday");
+            break;
+    case day5:
+            console.log("Thursday");
+            break;
+    case day6:
+            console.log("Friday");
+            break;
+    case day7:
+            console.log("saturday");
+            break;
+    default:
+            console.log("Invalid day");
+
+}
